f1d9c3fa6639ab215afcd52eb620e8b5cc471de9
Eileen <eileenburdekin@proton.me>
2023-11-07 14:53:18 -0800

    complete lab, changed to update server THEN dom

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 let addToy = false;
+//Set toyList as a variable. [] are reminders it's an array
+let toyList = [];
 
 const toyAPI = 'http://localhost:3000/toys'
 
@@ -21,35 +23,41 @@ document.addEventListener("DOMContentLoaded", () => {
 fetch(toyAPI)
   .then(res => res.json())
   //Loop through the array of toy objects
-  .then(toyData => toyData.forEach(toy => addNewToy(toy)))
+  .then(toyData => {
+    //Store returned data in array that was defined at top
+    //No matter what code does, toyList will be a globally avail array
+    toyList = toyData;
+    renderToys(toyList)
+  }
+  )
 
-function addNewToy(toy) {
+function renderToys(toyList) {
+  //Loop through array returned from json. For each object within the array, run addNewToy
+  const toyCollection = document.querySelector('#toy-collection')
+  toyCollection.innerHTML = ''
+  toyList.forEach(toyObj => addNewToy(toyObj))
+}
+
+function addNewToy(toyObj) {
   //Use the elements of the toy object to build the card (dot notation)
   const toyCollection = document.querySelector('#toy-collection')
-  const newToy = document.createElement('div')
-  newToy.className = 'card'
-  newToy.innerHTML = `
-  <h2>${toy.name}</h2>
-  <img src="${toy.image}" class="toy-avatar"/>
-  <p>Likes: ${toy.likes}</p>
+  const toyCard = document.createElement('div')
+  toyCard.className = 'card'
+  toyCard.innerHTML = `
+  <h2>${toyObj.name}</h2>
+  <img src="${toyObj.image}" class="toy-avatar"/>
+  <p>Likes: ${toyObj.likes}</p>
   `
   //Created like buttons outside of innerHTML so I can add event listeners as I go
   const likeButton = document.createElement('button')
   likeButton.className = 'like-btn'
-  likeButton.id = toy.id
+  likeButton.id = toyObj.id
   likeButton.textContent = 'Like'
-  //Create increaselikes function in-line so I have access to the toy.likes and toy.id on the DOM. Build callback function within for PATCH request
-  likeButton.addEventListener('click', function increaseLikes(e){
-    console.log(e.target)
-    // First increase likes in function
-    toy.likes += 1
-    //Then increase likes on DOM
-    newToy.querySelector('p').textContent = `Likes: ${toy.likes}`
-    //Then increase likes on server
-    increaseLikesServer(toy)
-  })
-  newToy.append(likeButton)
-  toyCollection.append(newToy)
+
+  //Add event listeners to Like buttons as they are created. 
+  likeButton.addEventListener('click', (e) => handleAddLike(toyObj));
+  toyCard.append(likeButton)
+  toyCollection.append(toyCard)
 }
 
 //CHALLENGE - Add Toy Info to Card / Add New Toy to Server
@@ -68,8 +76,6 @@ function handleSubmit(e) {
     image: e.target.image.value,
     likes: 0
   }
-  //Send cat object to addNewToy - ADD TO DOM
-  addNewToy(toyObj)
   //Call function for POST request - ADD TO JSON SERVER
   postNewToy(toyObj)
 }
@@ -79,32 +85,35 @@ function postNewToy(toyObj) {
   fetch(toyAPI, {
     method: 'POST',
     headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json"
+      "Content-Type": "application/json",
+      Accept: "application/json"
     },
     body: JSON.stringify(toyObj)
   })
-  .then(res => res.json)
-  .then(data => console.log(data))
+    .then(res => res.json)
+    .then(data => console.log(data))
 }
 
 //CHALLENGE - Update Likes on DOM via buttons / update Likes on server via PATCH
 
-//Added to function above 
-
-function increaseLikesServer(toyObj){
-  console.log('I will update the server')
-  fetch(`${toyAPI}/${toyObj.id}`,{
+function handleAddLike(toyObj) {
+  console.log('button clicked')
+  // First increase likes in function
+  const likes = toyObj.likes + 1;
+  console.log(likes)
+  fetch(`${toyAPI}/${toyObj.id}`, {
     method: 'PATCH',
     headers: {
       "Content-Type": "application/json",
       Accept: "application/json"
     },
-    body: JSON.stringify(toyObj)
+    body: JSON.stringify({likes})
   }
   )
-  .then(res => res.json())
-  //Console log updated object
-  .then(data => console.log(data))
+    .then(res => res.json())
+    //NOTHING IN THE ARGUMENT for the function below
+    .then(() => {
+      toyObj.likes = likes;
+      renderToys(toyList)
+    })
 }
-
